daf0699f51b2f6e96ec771e17886c677c23dbf6b
vfxsnake <jazzjoe@gmail.com>
2020-10-04 20:57:45 -0500

    alembic export function added

--- src/AssemblingTK/AssemblyUtils_lib.py
+++ src/AssemblingTK/AssemblyUtils_lib.py
@@ -17,3 +17,60 @@ class AssemblyUtils():
 
         RootParent = pyItem.getAllParents()[-1]
     
+
+
+    def ExportABC(self, startFrame , endFrame, ExportRootsString, outPath, additionalFlags=''):
+        """
+            build the JExport string containing all elements correctly formatted for the alembic export. 
+        """
+        # sets the frame range to export
+        rangeFlag = '-frameRange {0} {1} '.format(startFrame, endFrame)
+
+        # add custom flags to if needed
+        customFlags = '-uvWrite {0} -worldSpace -writeUVSets -dataFormat ogawa '.format(additionalFlags)
+
+   
+        rootFlat += '{0} '.format(ExportRootsString)
+
+    
+        fileFlag = '-file {0};'.format('C:/Users/vr-dev/Documents/maya/projects/default/cache/alembic/test.abc')
+        
+        
+        
+        jExport = rangeFlag + customFlags + rootFlag + fileFlag
+        
+        
+        pm.AbcExport(j=jExport)
+
+    def BuildRootString(self, RootElement):
+        return '-root {0} '.format(RootElement)
+
+    def SingleAbcExport(self, RootsList, startFrame, endFrame, outPath, AbcName, additionalFlags=None):
+        """
+            exports each element in root list as a single abc file 
+            to the spesified path 
+        """
+        rootString = ''
+        for element in RootsList:
+            rootString += self.BuildRootString(element)
+
+        
+        if rootString:
+            abcFile = outPath + "/{0}".format(AbcName)
+            self.ExportABC(startFrame, endFrame, rootString, outPath, additionalFlags)
+
+    
+    def MultipleAbcExport(self, RootsList, startFrame, endFrame, outPath, additionalFlags=None):
+        """
+            exports each elemen of the root list as a separate abc
+        """
+        for element in RootsList:
+            rootString = self.BuildRootString(element)
+            if rootString:
+                abcFile = outPath + "/{0}.abc".format(element)
+                self.ExportABC(startFrame, endFrame, rootString, abcFile, additionalFlags)
+
+
+
+
+        
