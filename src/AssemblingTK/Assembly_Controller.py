8badb5c120f8073f94b1aa76d78246d6494b1f62
unknown <vfxsnake>
2020-10-08 15:49:26 -0500

    last update

--- src/AssemblingTK/Assembly_Controller.py
+++ src/AssemblingTK/Assembly_Controller.py
@@ -5,7 +5,7 @@ import sys
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
-from PySide2.QtUiTools import QUiLoader
+
 
 
 class Assembly_Controller(QWidget):
